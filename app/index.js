9d757abe4673bc7ad515d1dc0fd6abaa6b91f6f4
Lucas García <lucas@bestiario.org>
2018-09-17 00:51:22 +0200

    general fixes

--- app/index.js
+++ app/index.js
@@ -1,16 +1,24 @@
 import { WebAPI } from './scripts/services/WebApi.js';
 import { GeoAPI } from './scripts/services/geo/GeoAPI.js';
+import { AppView } from './scripts/AppView.js';
 
-document.addEventListener("DOMContentLoaded", function() {
 
+const source_name = "static/json/spain.geojson";
 
-	const source_name = "static/json/spain.geojson";
+const webApi = new WebAPI();
+const geoAPI = new GeoAPI();
+const appView = new AppView({
+	el: '#content',
+	template: '<div class="app-view">APP VIEW</div>',
+	style: `#content {
+				background: #45f;
+			}`,
+	data: {}
+});
 
-	const webApi = new WebAPI();
-	const geoAPI = new GeoAPI();
-
+appView.ready(function() {
+	appView.render();
 	webApi.get(source_name).done(function(response) {
-		geoAPI.import(response).draw();
+		geoAPI.import(response).draw();	
 	});
-
 });
