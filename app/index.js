c15c67124bf8f630108267ccf145b9a411230dd0
Lucas García <lucas@bestiario.org>
2018-09-16 03:15:18 +0200

    change default data source

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ import { GeoAPI } from './scripts/services/geo/GeoAPI.js';
 document.addEventListener("DOMContentLoaded", function() {
 
 
-	const source_name = "spain-provinces.geojson";
+	const source_name = "static/json/spain.geojson";
 
 	const webApi = new WebAPI();
 	const geoAPI = new GeoAPI();
